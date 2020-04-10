4ea8c7304616c0b9f8e26e7cb8c45f67b68ed111
Stephen Odundo <steveremdi@gmail.com>
2020-04-10 16:24:30 +0300

    Simple doubler with snackbar showing value changes

--- app/src/main/java/com/example/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.kotlintest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        doubler.setOnClickListener {
+            val initialValue = Integer.parseInt(valToChange.text.toString())
+            val doubledValue = initialValue * 2
+            valToChange.text = doubledValue.toString()
+
+            Snackbar.make(findViewById(R.id.rootLayout), "Changed value $initialValue to $doubledValue", Snackbar.LENGTH_SHORT)
+        }
     }
 }
